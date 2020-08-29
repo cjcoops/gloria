becaa9a24b932c62584ac56032a96402b35113ec
chriscooper <cjcooper252@gmail.com>
2020-08-29 12:20:41 -0700

    Add prs logo

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,13 +3,34 @@ import Layout from '../components/layout'
 import Socials from '../components/socials'
 import Video from '../components/video'
 
-const MainPage = ({ data }) => (
-  <Layout>
-    <div>
-      <Socials />
-      <Video />
-    </div>
-  </Layout>
-)
+const MainPage = ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <Socials />
+        <Video />
+        <div className="prs-container">
+          <h4>GLOR1A is supported by PRS Foundation Women Make Music</h4>
+          <img
+            src={data.wmm.childImageSharp.fluid.src}
+            alt="Women Make Music"
+          />
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default MainPage
+
+export const query = graphql`
+  query {
+    wmm: file(relativePath: { eq: "wmm-logo.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 800) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+  }
+`
